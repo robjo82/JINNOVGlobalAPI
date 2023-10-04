db9a2b092ef89c4f18dfbecaf69819e131036578
Robin JOSEPH <robin.joseph@decathlon.com>
2023-10-04 15:48:22 +0200

    feat(app): Start SpringBoot app

---
+++ src/main/java/com/jinnov/globalAPI/JinnovGlobalApiApplication.java
@@ -0,0 +1,13 @@
+package com.jinnov.globalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinnovGlobalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinnovGlobalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinnov/globalAPI/JinnovGlobalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinnov.globalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinnovGlobalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
